66ae7efb0444463f1afb95c0ce820345484c22d1
Bryan Pitman <pitman.bryan@gmail.com>
2022-09-01 11:53:51 -0700

    started renderItems component

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,8 +1,40 @@
 import Box from './Box'
 import NewBoxForm from './NewBoxForm'
+import { v4 as uuid } from 'uuid';
+import { useState } from "react"
 
 //state for all boxes
-//similar to list? 
+//similar to list?
+function BoxList(){
+
+
+    const [boxes, setBoxes] = useState([]);
+
+    function renderItems() {
+      return (
+          <ul>
+            {boxes.map(box => (
+                <li key={box.id}>
+                  {box.width}: {box.qty}
+                </li>
+            ))}
+          </ul>
+      );
+    }
+
+    function addItem(box) {
+      let newBox= { ...box, id: uuid() };
+      setBoxes(boxes => [...boxes, newBox]);
+    }
+
+
+  return (
+    <div className="boxList">
+      <NewBoxForm  />
+
+    </div>
+  );
+}
 
 //render all Box components
 //render newBoxForm
